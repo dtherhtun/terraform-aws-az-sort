88d18e28d93c17001b00aaa39e29c4b19a5d7777
D Ther <dtherhtun.cw@gmail.com>
2022-11-08 13:47:25 +0700

    feat: region

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
       for i in range(length(var.subnet_data)) :
       [
         for data in var.subnet_data :
-        data.id if data.availability_zone == format("%s%s", "us-east-1", local.alphabetize[i])
+        data.id if data.availability_zone == format("%s%s", var.region, local.alphabetize[i])
       ]
     ]
   )
